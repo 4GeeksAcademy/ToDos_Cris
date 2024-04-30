caa8c328d6d235187cd9393713058c0fd30b7ab8
cris4crisland <160337227+cris4crisland@users.noreply.github.com>
2024-04-30 18:58:30 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,31 +7,32 @@ const Home = () => {
 	return (
 		<div className="container">
 			<h1>My Todos</h1>
-			<ul>
-				<li>
-					<input
-						type="text"
-						onChange={(e) => setInputValue(e.target.value)}
-						value={inputValue}
-						onKeyDown={(e) => {
-							if (e.key === "Enter") {
-								setTodos(todos.concat([inputValue]));
-								setInputValue("");
-							}
-						}}
-						placeholder="what do you need for the event?" />
-				</li>
+			<div>
+				<input
+					type="text"
+					onChange={(e) => setInputValue(e.target.value)}
+					value={inputValue}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							setTodos(todos.concat([inputValue]));
+							setInputValue("");
+						}
+					}}
+					placeholder="what do you need for the event?" />
+
 
 				{todos.map((item, index) => (
-					<li key={index} >
+
+					<li key={index} className="lista">
 						{item}{' '}
 						<i className="fas fa-trash-alt"
 							onClick={() =>
 								setTodos(todos.filter((t, currentIndex) => index !== currentIndex))}></i>
 					</li>))}
-			</ul>
+			</div>
+
 			<p>
-				Tienes {todos.lenght} tareas pendientes
+				Tienes {todos.length} tareas pendientes
 			</p>
 		</div>
 	);
